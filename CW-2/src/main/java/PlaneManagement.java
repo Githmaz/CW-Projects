b9c69e58f63f1aa75ba78e83dad1ae8149f68029
Githmaz <Githmaz1@gmail.com>
2024-03-13 13:34:13 +0530

    add Show Seat feature

--- CW-2/src/main/java/PlaneManagement.java
+++ CW-2/src/main/java/PlaneManagement.java
@@ -4,13 +4,34 @@ public class PlaneManagement {
     private static final int[][] seats = {new int[14], new int[12], new int[12], new int[14]};
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Welcome to the Plane Management application");
+        System.out.println("               _                          \n" +
+                "              | |                         \n" +
+                " __      _____| | ___ ___  _ __ ___   ___ \n" +
+                " \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n" +
+                "  \\ V  V /  __/ | (_| (_) | | | | | |  __/\n" +
+                "   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|");
+        System.out.println(" ____  __     ____  _  _  ___   \n" +
+                "(_  _)/  \\   (_  _)( )( )(  _)  \n" +
+                "  )( ( () )    )(   )__(  ) _)  \n" +
+                " (__) \\__/    (__) (_)(_)(___)  ");
+        System.out.println("______ _                   ___  ___                                                  _                       _ _           _   _             \n" +
+                "| ___ \\ |                  |  \\/  |                                                 | |                     | (_)         | | (_)            \n" +
+                "| |_/ / | __ _ _ __   ___  | .  . | __ _ _ __   __ _  __ _  ___ _ __ ___   ___ _ __ | |_    __ _ _ __  _ __ | |_  ___ __ _| |_ _  ___  _ __  \n" +
+                "|  __/| |/ _` | '_ \\ / _ \\ | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '_ ` _ \\ / _ \\ '_ \\| __|  / _` | '_ \\| '_ \\| | |/ __/ _` | __| |/ _ \\| '_ \\ \n" +
+                "| |   | | (_| | | | |  __/ | |  | | (_| | | | | (_| | (_| |  __/ | | | | |  __/ | | | |_  | (_| | |_) | |_) | | | (_| (_| | |_| | (_) | | | |\n" +
+                "\\_|   |_|\\__,_|_| |_|\\___| \\_|  |_/\\__,_|_| |_|\\__,_|\\__, |\\___|_| |_| |_|\\___|_| |_|\\__|  \\__,_| .__/| .__/|_|_|\\___\\__,_|\\__|_|\\___/|_| |_|");
 
         while (true) {
             titleDesign("MENU OPTIONS");
-            System.out.print("Please Enter your option : ");
+            System.out.println("\n 1) Buy a Seat");
+            System.out.println(" 2) Cancel a Seat");
+            System.out.println(" 3) Find First Available Seat");
+            System.out.println(" 4) Show Seating Plan");
+            System.out.print("\nPlease Enter your option : ");
             int option = scanner.nextInt();
             switch (option) {
+                case 0:
+                    return;
                 case 1:
                     buy_seat();
                     break;
@@ -20,9 +41,10 @@ public class PlaneManagement {
                 case 3:
                     find_first_available();
                     break;
-                case 0:
-
-                    return;
+                case 4:
+                    show_seating_plan();
+                default:
+                    System.out.println("\nInvalid Option, Try again");
             }
         }
     }
@@ -43,9 +65,9 @@ public class PlaneManagement {
                 if (seats[rowNumber][seatNumber-1] == 0) {
                     System.out.printf("\nSeat is sold %c%d , Thank you",row,seatNumber);
                     seats[rowNumber][seatNumber-1] = 1;
-                }else System.out.println("\nSorry, Seat is already Sold");
+                }else System.out.print("\nSorry, Seat is already Sold");
                 // Ask user try again or not
-                System.out.print("\nEnter 'Y' to buy a seat again , or Any-Other key to go to menu : ");
+                System.out.print("\n\nEnter 'Y' to buy a seat again , or Any-Other key to go to menu : ");
                 char option = scanner.next().charAt(0);
                 if (option != 'y' && option != 'Y') return;
                 // Invalid Input handler
@@ -55,7 +77,6 @@ public class PlaneManagement {
         }
     }
 
-
     // --------- Cancel Seat Method ---------- //
     public static void cancel_seat(){
         titleDesign("Cancel Seat");
@@ -64,13 +85,13 @@ public class PlaneManagement {
                 // Get user's row selection
                 System.out.print("\nEnter the Row (A,B,C,D) : ");
                 char row = scanner.next().charAt(0);
-                int rowNumber = row - 'A' + 1;
+                int rowNumber = row - 'A';
                 // Get user's seat number selection
                 System.out.printf("Enter the seat number (1 to %d) : ", seats[rowNumber].length);
                 int seatNumber = scanner.nextInt();
                 // Check the seat Sold or not
                 if (seats[rowNumber][seatNumber-1] == 1) {
-                    System.out.println("The Seat is available now , Thank you");
+                    System.out.println("\nThe Seat is available now , Thank you");
                     seats[rowNumber][seatNumber-1] = 0;
                 } else System.out.println("\nSorry, Seat is not Sold");
                 // Ask user try again or not
@@ -79,7 +100,7 @@ public class PlaneManagement {
                 if (option != 'y' && option != 'Y') return;
                 //invalid Input handler
             } catch (Exception e) {
-                System.out.println("Invalid Row or Seat number");
+                System.out.println("\nInvalid Row or Seat number");
             }
         }
     }
@@ -92,10 +113,23 @@ public class PlaneManagement {
             for(int j = 0; j < seats[i].length; j++) {
                 if (seats[i][j] == 0) {
                     System.out.printf("\nFirst Available Seat is : %c%d \n", (char)('A' + i), j+1);
-                    break outerLoop;
+                    break outerLoop; // Will break the outer for loop
                 }
             }
             if (i == seats.length-1) System.out.println("\nAll the Seats are Sold");
+        }
+        System.out.print("\nPress Any-Key to go back to menu : ");
+        scanner.next();
+    }
+
+    // --------- Show Seating Plan ---------- //
+    public static void show_seating_plan(){
+        titleDesign("Show Seating Plan");
+        for (int[] row : seats) {
+            for (int seat : row) {
+                System.out.print(seat == 0 ? " O " : " X ");
+            }
+            System.out.println();
         }
         System.out.print("\nPress Any-Key to go back to menu ");
         scanner.next();
